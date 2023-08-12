71725409a8751b1af509584ad7608af365ac898b
shifat.dev <adnan@lazertechnologies.com>
2023-08-12 14:11:41 +0600

    added icon button and configured global variables

---
+++ src/components/IconButton/IconButton.tsx
@@ -0,0 +1,24 @@
+import { type ButtonHTMLAttributes, type PropsWithChildren } from "react";
+
+export interface IconButton extends ButtonHTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  shouldFill?: boolean;
+}
+
+export default function IconButton({
+  children,
+  shouldFill,
+  className,
+  ...restProps
+}: PropsWithChildren<IconButton>) {
+  return (
+    <button
+      className={`flex h-10 w-10 items-center justify-center [&_svg]:h-6 [&_svg]:w-6 ${
+        shouldFill ? "[&_svg>*]:fill-primaryText" : ""
+      } ${className ? "" : ""}`}
+      {...restProps}
+    >
+      {children}
+    </button>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import { type Config } from "tailwindcss";
+
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        level1: "var(--level1)",
+        level2: "var(--level2)",
+        level3: "var(--level3)",
+        level4: "var(--level4)",
+        level1Hover: "var(--level1Hover)",
+        level2Hover: "var(--level2Hover)",
+        primaryText: "var(--primaryText)",
+        secondaryText: "var(--secondaryText)",
+        tertiaryText: "var(--tertiaryText)",
+        quaternaryText: "var(--quaternaryText)",
+        invertedPrimaryText: "var(--invertedPrimaryText)",
+        invertedTertiaryText: "var(--invertedTertiaryText)",
+      }
+    },
+  },
+  plugins: [],
+} satisfies Config;
